45f828cfd66509df7a680a6da2c2a127b5f2f827
ivvasch <ivvasch@gmail.com>
2021-01-13 18:20:01 +0300

    add Jpanel

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,24 @@
-import java.io.BufferedReader;
+import view.ViewFrameDeleteFromCatalog;
+
+import javax.swing.*;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Введите корневой каталог из которого Вы хотите удалить все файлы: ");
-        Path path = Paths.get(reader.readLine());
-
-        FindFromCatalog delete = new FindFromCatalog();
-        delete.deleteFromCatalog(path);
-        System.out.println("Все файлы в каталогах удалены!");
+        SwingUtilities.invokeLater(new Runnable() {
+                                       @Override
+                                       public void run() {
+                                           new ViewFrameDeleteFromCatalog();
+                                       }
+                                   }
+        );
+//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+//        System.out.println("Введите корневой каталог из которого Вы хотите удалить все файлы: ");
+//        Path path = Paths.get(reader.readLine());
+//
+//        FindFromCatalog delete = new FindFromCatalog();
+//        delete.deleteFromCatalog(path);
+//        System.out.println("Все файлы в каталогах удалены!");
 
     }
 }
